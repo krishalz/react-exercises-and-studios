1759074ec7695a379f3ac4b5dd6d7cdaa85bf7e4
kayhals <kayhals4@gmail.com>
2023-07-10 11:47:15 -0500

    Part 1 code added

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Excellent Book List";
+   let book1 = "https://images-na.ssl-images-amazon.com/images/I/417Dw75YqmL._AC_SX368_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/5101BKStCsL.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/41y5hXl9EjL.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Magic of Sea Glass by Jenny Hale" />
+         <img src={book2} alt="Doomsday Match (The Dresden Codex Book 1)" />
+         <img src={book3} alt="Girl With A Rose by Leslie Wolfe" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,9 @@
-export default function ChoresList () {
-   return <p>No content is here!</p>;
+import styles from "./ChoresList.module.css";
+export default function ChoresList() {
+  return (
+    <div className={styles.choresText}>
+      <h1 classNmae={styles.choresHeading}>This is my Chores heading</h1>
+      <p >No content is here!</p>
+    </div>
+  );
 }
