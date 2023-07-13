3584976f51a4ac7f4f88c941450246224aad1bf0
kayhals <kayhals4@gmail.com>
2023-07-13 13:40:36 -0500

    added simple css style changes

--- part1/studio/src/components/Description.js
+++ part1/studio/src/components/Description.js
@@ -2,19 +2,17 @@ import React from 'react'
 import styles from './Description.module.css';
 
 function RecipeAuthor() {
-    let pageTitle="Fantastic Brownie Recipe!";
     let authorLink = "https://sallysbakingaddiction.com/peanut-butter-cup-crunch-brownies-2/#tasty-recipes-77100"; //link to the recipe blog
     let authorPhoto = "https://sallysbakingaddiction.com/wp-content/uploads/2023/01/profile-sally.jpg"; //valid URL pointing to a photo of the author
-    let authorName = "found at Sally's Baking Addiction"; //authors full name
+    let authorName = "Sally's Baking Addiction"; //authors full name
 
    return (
-    <div>
-     <h3>{pageTitle}</h3>
-        <div className = {styles.recipeAuthorBlock}>
-            <img src={authorPhoto} alt = "Sallys Baking Addiction Brownie crunch recipe" className={styles.imageUpdates} />
-        </div>
-     <h3>{authorName}</h3>
-         <a href={authorLink}>Brownie Crunch Recipe</a> 
+    <div className = {styles.recipeAuthorBlock}>
+        <img src={authorPhoto} alt = "Sallys Baking Addiction Brownie crunch recipe" className={styles.imageUpdates} />
+        <div className = {styles.alignPhotos}>
+            <h3>{authorName}</h3>
+            <a href={authorLink}>Brownie Crunch Recipe Link</a> 
+         </div>
     </div>
    );
 }
@@ -23,8 +21,11 @@ class RecipeDescription extends React.Component {
     render() {
       return (
         <div>
-          <h3>Fantastic Brownie Crunch Recipe!</h3>
-          <p>This brownie crunch recipe is sooo delish!  &#x1F600;</p>
+            <div>
+                <h3>Fantastic Brownie Crunch Recipe!</h3>
+                <p>This brownie crunch recipe is sooo delish!  &#x1F600;</p>
+            </div>      
+        <RecipeAuthor />
         </div>
       );
     }
--- part1/studio/src/components/Photos.js
+++ part1/studio/src/components/Photos.js
@@ -3,7 +3,7 @@ import styles from './Description.module.css';
 
 export default function RecipePhoto() {
   return (
-    <div>
+    <div className={styles.alginPhotos}>
        <img src="https://sallysbakingaddiction.com/wp-content/uploads/2012/01/Peanut-Butter-Cup-Crunch-Brownies-7-400x400.jpg" alt="recipe photo" className = {styles.imageUpdates} />   
     </div>
   )
