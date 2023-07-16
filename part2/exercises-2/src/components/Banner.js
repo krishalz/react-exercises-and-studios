5abac4b92fdf7cb75d65c5e7c8dee1e560c1c0f6
kayhals <kayhals4@gmail.com>
2023-07-16 17:27:03 -0500

    completed coding exercise-2

--- part2/exercises-2/src/components/Banner.js
+++ part2/exercises-2/src/components/Banner.js
@@ -2,8 +2,9 @@ import './styles.css';
 
 function Banner(){
    return(
-      <h1>Hello, World!</h1>
-   );
+      <h1 className="header" >The Amazing Ocean!</h1>
+
+  );
 }
 
 export default Banner;
--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,27 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
+const listItem = oceans.map(ocean => 
+   <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <img src={ocean.image} alt={ocean.name} className="img"/>
+      <h1>{ocean.name}</h1>
+      <h2>Fun Facts</h2>
+      <h3>
+         <ul>{ocean.fact1}</ul>
+         <ul>{ocean.fact2}</ul>
+         <ul>{ocean.fact3}</ul>
+      </h3>
+      <Button />
+   </div>
+);
 
 function Profile()
 {
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+         <ul>
+            {listItem}
+         </ul>
    );
 }
 
