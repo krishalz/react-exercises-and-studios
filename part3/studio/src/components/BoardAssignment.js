32cf3e89f5c23ee534aaaad2d09d30259aa803ca
kayhals <kayhals4@gmail.com>
2023-08-04 11:56:01 -0500

    completed React P3 Studio

--- part3/studio/src/components/BoardAssignment.js
+++ part3/studio/src/components/BoardAssignment.js
@@ -1,15 +1,38 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const airFryer = {
+      label: "Air Fryer Recipes",
+      value: 1
+   };
+   const chickenRecipes = {
+      label: "Chicken Recipes",
+      value: 2
+   };
+   const drummond = {
+      label: "The Drummond Farm",
+      value: 3
+   };
 
+   const [boardName, setName] = useState("no boards yet!");
+   const boards = [airFryer, chickenRecipes, drummond];
+  
    const handleChange = (event) => {
-   }
+      const selectedValue = event.target.value;
+      const selectedBoard = boards.find((board) => board.value === parseInt(selectedValue));
+      setName(selectedBoard ? selectedBoard.label : "no boards yet!");
+    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
       <select value={boardName} onChange={handleChange}>
+        <option value="no boards yet!">Select a board</option>
+        {boards.map((board) => (
+          <option key={board.value} value={board.value}>
+            {board.label}
+          </option>
+        ))}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.js
+++ part3/studio/src/components/Recipe.js
@@ -1,21 +1,21 @@
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://www.thepioneerwoman.com/ree-drummond-life/";
+   let authorPhoto = "https://hips.hearstapps.com/hmg-prod/images/scallop-photo-web3-64c02016792be.jpg?crop=1.00xw:1.00xh;0,0&resize=360:*";
+   let authorName = "Ree Drummond";
 
    return (
       <div>
          <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}>Recipe Website</a> 
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["chicken breasts","eggs","flour","parmesan","marinara sauce","breadcrumbs","spices (basil, black pepper, Italian seasoning, garlic powder)","mozarella cheese"];
    return(
       <div>
          <h3>Recipe Ingredients</h3>
@@ -25,6 +25,9 @@ const RecipeIngredients = () => {
             <li>{ingredients[2]}</li>
             <li>{ingredients[3]}</li>
             <li>{ingredients[4]}</li>
+            <li>{ingredients[5]}</li>
+            <li>{ingredients[6]}</li>
+            <li>{ingredients[7]}</li>
          </ul>
       </div>
    );
@@ -34,8 +37,8 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Air Fryer Chicken Parmesan</h1>
+            <p>Super crispy, parmesan-crusted chicken cutlets</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -47,7 +50,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+          <img src="https://hips.hearstapps.com/hmg-prod/images/air-fryer-chicken-parmesan-recipe-1-1653592008.jpg?crop=0.941xw:0.941xh;0.0535xw,0.0587xh&resize=1200:*" alt=""/>
    );
 }
 
